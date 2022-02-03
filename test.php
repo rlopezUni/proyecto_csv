62e78af540bc0df853bf199aa781d4221c527b13
cesar <19020083@alumnos.univer-gdl.edu.mx>
2022-02-03 16:49:56 -0600

    Lo del array ahí esta.

--- test.php
+++ test.php
@@ -5,7 +5,6 @@
 
 //Leer todos los archivos de una carpeta
 
-/*
 
 function listarArchivos( $path ){
     // Abrimos la carpeta que nos pasan como parámetro
@@ -30,19 +29,21 @@ function listarArchivos( $path ){
 
 listarArchivos("./archivos/");
 
-*/
 
 //Leer un arhivo csv
 
-/*
+
 # La longitud máxima de la línea del CSV. Si no la sabes,
 # ponla en 0 pero la lectura será un poco más lenta
 $longitudDeLinea = 1000;
 $delimitador = ","; # Separador de columnas
 $caracterCircundante = '"'; # A veces los valores son encerrados entre comillas
-$nombreArchivo = "3.csv";
+$nombreArchivo = "./archivos/3.csv";
+$NA = "./archivos/Ejemplo.csv";
+$prueba = array();
 # Abrir el archivo
 $archivo = fopen($nombreArchivo, "r");
+$A = fopen($NA, "a");
 if (!$archivo) {
     exit("No se puede abrir el archivo $nombreArchivo");
 }
@@ -61,38 +62,49 @@ while (($fila = fgetcsv($archivo, $longitudDeLinea, $delimitador, $caracterCircu
 
         if($numeroDeColumna == 0)
         {
+          echo "<br><br><br>";
           echo "Inicio Alumno<br>";
           echo "Matricula: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
         }
         else if($numeroDeColumna == 1)
         {
           echo "Nombre: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
         }
         else if($numeroDeColumna == 2)
         {
           echo "Apellido: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
         }
         else if($numeroDeColumna == 3)
         {
           echo "Campus: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
         }
         else if($numeroDeColumna == 4)
         {
           echo "Correo: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
         }
         else if($numeroDeColumna == 5)
         {
           echo "Contraseña: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
         }
         else if($numeroDeColumna == 6)
         {
           echo "Correo Personal: $columna\n";
+          $prueba[$numeroDeColumna] = $columna;
           echo "Fin De un alumno <br>";
         }
-       
-        
         echo "</pre>";
       }
+
+    }
+    foreach($prueba as $test)
+    {
+        fputcsv($A, (array) $test);
     }
     # Para separar la impresión
     echo "\n\n";
@@ -101,4 +113,5 @@ while (($fila = fgetcsv($archivo, $longitudDeLinea, $delimitador, $caracterCircu
 }
 # Al finar cerrar el archivo
 fclose($archivo);
-*/
+fclose($A);
+
